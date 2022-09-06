6a72aa164108e23be769f0b68caf50176d9a6baa
Igor Padoim <igorpadoim@gmail.com>
2022-09-05 22:57:08 -0300

    Fix instance contact link

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer class="footer">
       <div class="content has-text-centered">
         <p>
-          <strong><?php echo $title; ?></strong> by <a href="<?php echo constant( "INSTANCE_ADMIN_EMAIL" ); ?>"><?php echo constant( "INSTANCE_ADMIN_NAME" ); ?></a>
+          <strong><?php echo $title; ?></strong> by <a href="mailto:<?php echo constant( "INSTANCE_ADMIN_EMAIL" ); ?>"><?php echo constant( "INSTANCE_ADMIN_NAME" ); ?></a>
         </p>
         <p>
           <a href="<?php echo constant("APP_REPO"); ?>">
