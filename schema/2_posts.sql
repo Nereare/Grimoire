eb50c6b4fb7de7609893f57735f5b03784650612
Igor Padoim <igorpadoim@gmail.com>
2022-09-06 22:15:18 -0300

    Finish posts schema This defines the "Home" group pages' tables

--- schema/2_posts.sql
+++ schema/2_posts.sql
@@ -6,6 +6,36 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `title`         VARCHAR(100) NOT NULL,
   `tags`          VARCHAR(255),
   `categories`    VARCHAR(255),
-  `body`          TEXT NOT NULL,
+  `body`          MEDIUMTEXT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `books` (
+  `id`            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`          VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `chapters` (
+  `id`            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `author`        INT UNSIGNED NOT NULL,
+  `book`          INT UNSIGNED NOT NULL,
+  `published`     DATE NOT NULL,
+  `edited`        DATE,
+  `title`         VARCHAR(100) NOT NULL,
+  `body`          MEDIUMTEXT NOT NULL,
+  `previous`      INT UNSIGNED,
+  `next`          INT UNSIGNED,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `pages` (
+  `id`            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `author`        INT UNSIGNED NOT NULL,
+  `name`          VARCHAR(100) NOT NULL,
+  `published`     DATE NOT NULL,
+  `edited`        DATE,
+  `title`         VARCHAR(100) NOT NULL,
+  `body`          MEDIUMTEXT NOT NULL,
   PRIMARY KEY (`id`)
 );
