b054cf46940f9b64497d95e5a265e66380136be7
Igor Padoim <igorpadoim@gmail.com>
2023-01-04 08:34:20 -0300

    Update year

--- meta.php
+++ meta.php
@@ -2,7 +2,7 @@
 define("APP_NAME",          "Grimoire");
 define("APP_REPO",          "https://github.com/Nereare/Grimoire");
 define("APP_AUTHOR",        "Igor Padoim");
-define("APP_YEAR",          "2019-2022");
+define("APP_YEAR",          "2019-2023");
 define("APP_BIRTHDAY",      "2019-08-12");
 define("APP_LICENSE_NAME",  "Hippocratic License 3.0");
 define("APP_LICENSE_URI",   "https://firstdonoharm.dev/");
